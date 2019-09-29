c8b98fb9926751dbe088d797b38778da3aaf9aef
JIA MING MA <jma007@citymail.cuny.edu>
2019-09-29 19:57:38 -0400

    finished layout for now, need to put in real information later

--- src/jquery.js
+++ src/jquery.js
@@ -1,4 +1,24 @@
 $(document).ready(function() {
+  $('#discord').fadeOut();
+  $('#gmail').fadeOut();
+  $('#outlook').fadeOut();
+});
+
+function copy(text, id) {
+  var textArea = document.createElement("textarea");
+  textArea.value = text;
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+  document.execCommand('copy');
+  document.body.removeChild(textArea);
+  id = '#' + id;
+  $(id).fadeIn(1000);
+  $(id).fadeOut(2500);
+}
+
+
+$(document).ready(function() {
   $(window).scroll(function() {
       var scrollTop = $(window).scrollTop();
       var height = $(window).height();
