b3b21bd6ebdb5ff52679016a1d64fb925a0ba5cb
JIA MING MA <jma007@citymail.cuny.edu>
2019-09-30 02:00:13 -0400

    added navbar highlighter based on scrollTop position, still missing personal projects information

--- src/jquery.js
+++ src/jquery.js
@@ -1,15 +1,58 @@
 $(document).ready(function() {
+  // init
   $('#github').fadeOut();
   $('#discord').fadeOut();
   $('#gmail').fadeOut();
   $('#outlook').fadeOut();
   $('#youtube').fadeOut();
+  highlightNav();
 
   if (screen.width <= 699) {
     alert("It seems like you're on a mobile phone :)\nPlease use a web browser like Chrome instead, thanks!");
   }
+
+  // highlight navbar on scroll
+  $(window).scroll(function() {
+    highlightNav();
+  });
+
+  // highlight navbar on hover
+  $('.nava').hover(
+    function() {
+      $(this).css('color', '#9e81c5');
+      $(this).css('font-weight', 'bold');
+    },
+    function() {
+      $(this).css('color', '#ffffff');
+      $(this).css('font-weight', 'none');
+    }
+  );
+
 });
 
+function highlightNav() {
+  var scrollMax = $(document).height() - $(window).height();
+  $('.nava').css('color', '#ffffff');
+  if ($(window).scrollTop() < scrollMax * 0.1) {
+    $('#a-home').css('color', '#8A2BE2');
+    $('#a-home').css('font-weight', 'bold');
+  } else if ($(window).scrollTop() >= scrollMax * 0.1 && $(window).scrollTop() < scrollMax * 0.275) {
+    $('#a-intro').css('color', '#8A2BE2');
+    $('#a-intro').css('font-weight', 'bold');
+  } else if ($(window).scrollTop() >= scrollMax * 0.275 && $(window).scrollTop() < scrollMax * 0.4225) {
+    $('#a-resume').css('color', '#8A2BE2');
+    $('#a-resume').css('font-weight', 'bold');
+  } else if ($(window).scrollTop() >= scrollMax * 0.4225 && $(window).scrollTop() < scrollMax * 0.53) {
+    $('#a-projects').css('color', '#8A2BE2');
+    $('#a-projects').css('font-weight', 'bold');
+  } else if ($(window).scrollTop() >= scrollMax * 0.53 && $(window).scrollTop() < scrollMax * 0.92) {
+    $('#a-game').css('color', '#8A2BE2');
+    $('#a-game').css('font-weight', 'bold');
+  } else {
+    $('#a-contact').css('color', '#8A2BE2');
+    $('#a-contact').css('font-weight', 'bold');
+  }
+}
 
 function copy(text, id) {
   var textArea = document.createElement("textarea");
@@ -23,12 +66,3 @@ function copy(text, id) {
   $(id).fadeIn(1000);
   $(id).fadeOut(2500);
 }
-
-
-$(document).ready(function() {
-  $(window).scroll(function() {
-      var scrollTop = $(window).scrollTop();
-      var height = $(window).height();
-      $(".fade-scroll").css({"opacity": ((height-scrollTop) / height)});
-  });
-});
