f7240825d0dbd155f3c92e1713bf858fef1a1173
Jia Ming <jma8774@bths.edu>
2020-12-24 02:09:46 -0500

    title done? havent tested mobile

--- custom_js.js
+++ custom_js.js
@@ -1,25 +1,79 @@
-// Detect Mobile
-var isMobile = false;
+// Sliding Helper Functions
+$.fn.isInViewport = function() {
+  var elementTop = $(this).offset().top;
+  var elementBottom = elementTop + $(this).outerHeight();
 
+  var viewportTop = $(window).scrollTop();
+  var viewportBottom = viewportTop + $(window).height();
+
+  return elementBottom > viewportTop && elementTop < viewportBottom;
+};
+function add_slide_in(tag, idx) {
+  $(tag).each(function(i, el) {
+    if ($(el).isInViewport()) {
+      setTimeout(function() { 
+        $(el).addClass("slide-up");
+      }, 150 * i);
+    }
+  });
+}
+
+var atTop = false;
+
+// Document.Ready()
 jQuery(function() {
+  // Initial
+    // Detect Mobile
+  var isMobile = false;
   if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
     isMobile = true;
   }
-});
+    // Viewport
+  viewportHeight = $(window).height();
+  $("#title").css("margin-top", (viewportHeight-$("#title").outerHeight()) * 0.25);
+  $("#title").css("margin-bottom", (viewportHeight-$("#title").outerHeight()) * 0.65);
+    // Determine Mobile/Desktop Navbar
+  if($(window).width() < 800 || isMobile){
+    $('.desktop-nav').addClass("d-none");
+    $('.mobile-nav').removeClass("d-none");
+  }
+    // Slide In
+  let tags = ['.title-anim', '.part1', '.part2', '.part3', '.part4', '.part5']
+  tags.forEach(add_slide_in)
+  $(".left-nav-anim").each(function(i, el) {
+    setTimeout(function() { 
+      $(el).addClass("slide-down");
+    }, 150 * i);
+  });
+  $(".right-nav-anim").each(function(i, el) {
+    setTimeout(function() { 
+      $(el).addClass("slide-down");
+    }, 300 + (150 * i));
+  });
+  setTimeout(function() { 
+    $('.mobile-right-nav-anim').addClass("slide-down");
+  }, 300);
 
-// Enable popovers and tooltips
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-})
+  
+  // On Resize
+  $(window).on('resize', function() {
+    // Determine Mobile/Desktop Navbar
+    if($(window).width() < 800 || isMobile) {
+      $('.desktop-nav').addClass("d-none");
+      $('.mobile-nav').removeClass("d-none");
+    } else {
+      $('.desktop-nav').removeClass("d-none");
+      $('.mobile-nav').addClass("d-none");
+    }
+    // Margin/Padding of Title
+    viewportHeight = $(window).height();
+    $("#title").css("margin-top", (viewportHeight-$("#title").outerHeight()) * 0.25);
+    $("#title").css("margin-bottom", (viewportHeight-$("#title").outerHeight()) * 0.65);
+  });
 
-$(function () {
-  $('[data-toggle="popover"]').popover()
-})
 
-// Navbar #################################################################################
-  // Navbar Scrolling Options
-var lastScrollTop = Number.MAX_VALUE;
-jQuery(function() {
+  // On Scroll
+  var lastScrollTop = Number.MAX_VALUE;
   $(window).on("scroll", function (event) {
     // Popover
     $('#btn-heart').popover('hide');
@@ -41,82 +95,31 @@ jQuery(function() {
       $(".navbar").removeClass("nav-trans-in");
     }
     lastScrollTop = curScrollTop;
+    // Slide In
+    let tags = ['.title-anim', '.part1', '.part2', '.part3', '.part4', '.part5']
+    tags.forEach(add_slide_in)
   });
-});
-  // Navbar Width Options
-jQuery(function() {
-  if($(window).width() < 800 || isMobile){
-    $('.desktop-nav').addClass("d-none");
-    $('.mobile-nav').removeClass("d-none");
-  }
-});
-jQuery(function() {
-  $(window).on('resize', function() {
-    if($(window).width() < 800 || isMobile) {
-      $('.desktop-nav').addClass("d-none");
-      $('.mobile-nav').removeClass("d-none");
-    } else {
-      $('.desktop-nav').removeClass("d-none");
-      $('.mobile-nav').addClass("d-none");
-    }
-  });
-});
-  // GitHub Button
-jQuery(function() {
+
+
+  // On Click
+    // GitHub Button
   $("#btn-github").on("click", function() {
     window.open('https://github.com/jma8774', '_blank');
   });
-});
-  // Resume Button
-jQuery(function() {
+    // Resume Button
   $(".btn-resume").on("click", function() {
     window.open('resume.pdf', '_blank');
   });
 });
 
-// Slide In #################################################################################
-  // Check if element is visible in viewport
-$.fn.isInViewport = function() {
-  var elementTop = $(this).offset().top;
-  var elementBottom = elementTop + $(this).outerHeight();
 
-  var viewportTop = $(window).scrollTop();
-  var viewportBottom = viewportTop + $(window).height();
-
-  return elementBottom > viewportTop && elementTop < viewportBottom;
-};
-  // Given a tag, make them slide in order
-function add_come_in(tag, idx) {
-  $(tag).each(function(i, el) {
-    if ($(el).isInViewport()) {
-      setTimeout(function() { 
-        $(el).addClass("come-in");
-      }, 150 * i);
-    }
-  });
-}
-  // Initial Slide Ins
-jQuery(function() {
-  let tags = ['.left-nav-anim','.part1', '.part2', '.part3', '.part4', '.part5']
-  // mobile right nav
-  setTimeout(function() { 
-    $('.mobile-right-nav-anim').addClass("come-in");
-  }, 300);
-  // desktop right nav
-  $(".right-nav-anim").each(function(i, el) {
-    setTimeout(function() { 
-      $(el).addClass("come-in");
-    }, 300 + (150 * i));
-  });
-  tags.forEach(add_come_in)
-});
-  // Scrolling Slide Ins
-jQuery(function() {
-  $(window).on("scroll", function (event) {
-    let tags = ['.part1', '.part2', '.part3', '.part4', '.part5']
-    tags.forEach(add_come_in)
-  });
-});
+// Enable popovers and tooltips
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
 
 
   
